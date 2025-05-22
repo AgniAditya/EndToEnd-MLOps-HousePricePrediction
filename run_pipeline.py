d08cec901f527630b959d773bb6ff876f80c4bfa
AgniAditya <agniaditya05@gmail.com>
2025-05-22 09:59:07 +0530

    MLFlow Experiment Tracker implemented

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,11 +1,6 @@
 from pipeline.training_pipeline import trianingpipeline
-from config.mlflow_setup import setup_mlflow
 
 if __name__ == "__main__":
-    """
-    Stepup the MLFlow
-    """
-    setup_mlflow()
     """
     Run the Pipeline
     """
--- steps/evalute_model.py
+++ steps/evalute_model.py
@@ -8,7 +8,7 @@ import pandas as pd
 import mlflow
 from zenml import step
 
-@step
+@step(enable_cache=False)
 def evalutemodel(model: RandomForestRegressor,
     X_test: pd.DataFrame,
     y_test: pd.Series) -> None:
@@ -25,11 +25,10 @@ def evalutemodel(model: RandomForestRegressor,
         rmse = np.sqrt(((y_test - rf_model_preditctions) ** 2).mean())
         mape = mean_absolute_percentage_error(y_test, rf_model_preditctions)
 
-        with mlflow.start_run(nested=True):
-            mlflow.log_metric("r2_score", r2_score_rf)
-            mlflow.log_metric("rmse", rmse)
-            mlflow.log_metric("mae", mae)
-            mlflow.log_metric("mape", mape)
+        mlflow.log_metric("r2_score", r2_score_rf)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("mape", mape)
 
         logging.info("Metrics logged to MLflow.")
     except Exception as e:
